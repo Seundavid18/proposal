067b8e0587f5c5aa7339ad5b37af161baf643fb1
Seundavid18 <oyewumidavid84@gmail.com>
2024-07-15 18:59:10 +0100

    updated code

--- src/proposal.js
+++ src/proposal.js
@@ -6,13 +6,13 @@ const Proposal = () => {
         alert("Yay! I'm so happy! I love you so much");
     };
 
-    // const handleNo = () => {
-    //     alert("Oh no! Maybe next time.");
-    // };
+    const handleNo = () => {
+        alert("Oh no! why?.");
+    };
 
     return (
         <div className="proposal-container">
-            <h1>To my baby Oluwaseun Deborah Jegede</h1>
+            <h1>To my baby Oluwadamilola Amora</h1>
             <p>
                 Every moment spent imagining and thinking about you feels like a beautiful dream come true. 
                 Your smile brightens my day and your voice makes my heart race. 
@@ -22,7 +22,7 @@ const Proposal = () => {
                 Would you do me the honor of being my girlfriend and make me the happiest person in the world?
             </p>
             <button className="yes-button" onClick={handleYes}>Yes</button>
-            {/* <button className="no-button" onClick={handleNo}>No</button> */}
+            <button className="no-button" onClick={handleNo}>No</button>
         </div>
     );
 };
