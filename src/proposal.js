374c05e4a9a0dabb17638022d9bc881bc970e91e
Seundavid18 <oyewumidavid84@gmail.com>
2024-11-05 22:53:43 +0100

    Remilekun

--- src/proposal.js
+++ src/proposal.js
@@ -12,14 +12,14 @@ const Proposal = () => {
 
     return (
         <div className="proposal-container">
-            <h1>To my baby Ifeoluwa Olaitan Baliqis</h1>
+            <h1>To my baby Remilekun Victoria Oyindamola Adebake</h1>
             <p>
                 Every moment spent imagining and thinking about you feels like a beautiful dream come true. 
                 Your smile brightens my day and your voice makes my heart race. 
                 I can't imagine my life without you, and I want to cherish every moment together.
             </p>
             <p>
-                Would you do me the honor of being my girlfriend and make me the happiest person in the world?
+                So, to make this very official, Oyindamola, Would you do me the honor of being my girlfriend?
             </p>
             <button className="yes-button" onClick={handleYes}>Yes</button>
             <button className="no-button" onClick={handleNo}>No</button>
